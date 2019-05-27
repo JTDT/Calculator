4bb0d109119a7656ec45360c971446dd95d681b3
Jenny Tran <jenny.thuy.dung.tran@post.au.dk>
2019-05-27 10:17:09 +0200

    Add project files.

---
+++ Calculator.UnitTest/CalculatorUnitTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculator.UnitTest
+{
+    [TestFixture]
+    public class CalculatorUnitTests
+    {
+        private Calculator _uut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _uut = new Calculator();
+        }
+
+        [TestCase(2, 2, 4)]
+        [TestCase(2.6, -2, 0.6)]
+        [TestCase(-2, -6, -8)]
+        public void CalculatorAdd_AddAAndB_returnsResult(double a, double b, double result)
+        {
+            Assert.That(_uut.Add(a, b), Is.EqualTo(result).Within(0.01));
+        }
+
+        [TestCase(2, 2, 0)]
+        [TestCase(-4, -2, -2)]
+        [TestCase(2.5, 0.5, 2)]
+        public void CalculatorSubtract_SubtractAWithB_returnsResult(double a, double b, double result)
+        {
+            Assert.That(_uut.Subtract(a, b), Is.EqualTo(result).Within(0.01));
+        }
+
+        [TestCase(2, 5, 10)]
+        [TestCase(-2, 4, -8)]
+        [TestCase(3.5, 3.5, 12.25)]
+        public void CalculatorMultiply_MultiplyAAndB_returnsResult(double a, double b, double result)
+        {
+            Assert.That(_uut.Multiply(a, b), Is.EqualTo(result).Within(0.01));
+        }
+
+        [TestCase(10, 1, 10)]
+        [TestCase(-20, 1, -20)]
+        public void CalculatorPower_ExpToX_returnsResult(double x, double exp, double result)
+        {
+            Assert.That(_uut.Power(x, exp), Is.EqualTo(result).Within(0.01));
+        }
+
+
+    }
+}
---
+++ Calculator/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Power(double x, double exp)
+        {
+            double result;
+            result = Math.Pow(x, exp);
+
+            return result;
+        }
+
+    }
+}
